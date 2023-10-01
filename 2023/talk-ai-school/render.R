82023ea4863f84d9ed0af3c2c1e12020bfeb46d0
Sebastian Sauer <ssauer@posteo.de>
2023-10-01 08:32:10 +0200

    update

---
+++ 2023/talk-ai-school/render.R
@@ -0,0 +1,1 @@
+quarto::quarto_render("/Users/sebastiansaueruser/Publikationen/blog_ses/sesa-blog/static/slides/talk-ai-school/index.qmd")
--- talk-ai-school/render.R
+++
@@ -1,1 +0,0 @@
-quarto::quarto_render("/Users/sebastiansaueruser/Publikationen/blog_ses/sesa-blog/static/slides/talk-ai-school/index.qmd")
